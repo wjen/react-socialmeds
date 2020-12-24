7a9e21e6e3b9db420d544acd51d680ab554c0f8f
Wen Jen <wjen1214@gmail.com>
2020-12-23 22:27:56 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+const { MONGODB } = require('./config.js');
+const typeDefs = gql`
+  type Query {
+    sayHi: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHi: () => 'Hello World',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('mongodb connected');
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`server running at ${res.url}`);
+  });
