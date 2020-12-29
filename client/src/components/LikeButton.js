a23698db1fc1c629a1b8a82396fcc17d58a0bc73
Wen Jen <wjen1214@gmail.com>
2020-12-28 20:23:10 -0800

    set up like post

---
+++ client/src/components/LikeButton.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { gql, useMutation } from '@apollo/client';
+import { Button, Icon, Label } from 'semantic-ui-react';
+
+function LikeButton({ user, post }) {
+  let { id, likeCount, likes } = post;
+
+  const [liked, setLiked] = useState(false);
+  useEffect(() => {
+    if (user && likes.find((like) => like.username === user.username)) {
+      setLiked(true);
+    } else {
+      setLiked(false);
+    }
+  }, [likes, user]);
+
+  const [likePost] = useMutation(LIKE_POST_MUTATION, {
+    variables: { postId: id },
+  });
+
+  const likeButton = user ? (
+    liked ? (
+      <Button color="teal">
+        <Icon name="heart" />
+      </Button>
+    ) : (
+      <Button color="teal" basic>
+        <Icon name="heart" />
+      </Button>
+    )
+  ) : (
+    <Button as={Link} to="/login" color="teal" basic>
+      <Icon name="heart" />
+    </Button>
+  );
+  return (
+    <Button as="div" labelPosition="right" onClick={likePost}>
+      {likeButton}
+      <Label basic color="teal" pointing="left">
+        {likeCount}
+      </Label>
+    </Button>
+  );
+}
+
+const LIKE_POST_MUTATION = gql`
+  mutation likePost($postId: ID!) {
+    likePost(postId: $postId) {
+      id
+      likes {
+        id
+        username
+      }
+      likeCount
+    }
+  }
+`;
+
+export default LikeButton;
