70c98eec5c3dfbccf7ff577d6b8c07225cd6fa24
Wen Jen <wjen1214@gmail.com>
2020-12-29 18:37:55 -0800

    add delete post functionality

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -3,25 +3,29 @@ import { gql, useMutation } from '@apollo/client';
 import { Icon, Confirm, Button } from 'semantic-ui-react';
 import { FETCH_POSTS_QUERY } from '../util/graphql';
 
-function DeleteButton({ postId, callback }) {
+function DeleteButton({ postId, callback, commentId }) {
   const [confirmOpen, setConfirmOpen] = useState(false);
 
-  const [deletePost] = useMutation(DELETE_POST_MUTATION, {
+  const mutation = commentId ? DELETE_COMMENT_MUTATION : DELETE_POST_MUTATION;
+
+  const [deletePostOrMutation] = useMutation(mutation, {
     update(proxy) {
       setConfirmOpen(false);
-      const data = proxy.readQuery({
-        query: FETCH_POSTS_QUERY,
-      });
-      let newData = data.getPosts.filter((p) => p.id !== postId);
-      proxy.writeQuery({
-        query: FETCH_POSTS_QUERY,
-        data: {
-          ...data,
-          getPosts: {
-            newData,
+      if (!commentId) {
+        const data = proxy.readQuery({
+          query: FETCH_POSTS_QUERY,
+        });
+        let newData = data.getPosts.filter((p) => p.id !== postId);
+        proxy.writeQuery({
+          query: FETCH_POSTS_QUERY,
+          data: {
+            ...data,
+            getPosts: {
+              newData,
+            },
           },
-        },
-      });
+        });
+      }
       if (callback) callback();
     },
     onError(error) {
@@ -29,6 +33,7 @@ function DeleteButton({ postId, callback }) {
     },
     variables: {
       postId,
+      commentId,
     },
   });
   return (
@@ -44,7 +49,7 @@ function DeleteButton({ postId, callback }) {
       <Confirm
         open={confirmOpen}
         onCancel={() => setConfirmOpen(false)}
-        onConfirm={deletePost}
+        onConfirm={deletePostOrMutation}
       />
     </>
   );
@@ -53,6 +58,21 @@ function DeleteButton({ postId, callback }) {
 const DELETE_POST_MUTATION = gql`
   mutation deletePost($postId: ID!) {
     deletePost(postId: $postId)
+  }
+`;
+
+const DELETE_COMMENT_MUTATION = gql`
+  mutation deleteComment($postId: ID!, $commentId: ID!) {
+    deleteComment(postId: $postId, commentId: $commentId) {
+      id
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+      commentCount
+    }
   }
 `;
 export default DeleteButton;
