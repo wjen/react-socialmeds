638852b63daf87f64a456c0decabaff99b615eec
Wen Jen <wjen1214@gmail.com>
2020-12-29 17:17:40 -0800

    set up post refresh after deleting post

---
+++ client/src/components/DeleteButton.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
+import { Icon, Confirm, Button } from 'semantic-ui-react';
+import { FETCH_POSTS_QUERY } from '../util/graphql';
+
+function DeleteButton({ postId, callback }) {
+  const [confirmOpen, setConfirmOpen] = useState(false);
+
+  const [deletePost] = useMutation(DELETE_POST_MUTATION, {
+    update(proxy) {
+      setConfirmOpen(false);
+      const data = proxy.readQuery({
+        query: FETCH_POSTS_QUERY,
+      });
+      let newData = data.getPosts.filter((p) => p.id !== postId);
+      proxy.writeQuery({
+        query: FETCH_POSTS_QUERY,
+        data: {
+          ...data,
+          getPosts: {
+            newData,
+          },
+        },
+      });
+      if (callback) callback();
+    },
+    onError(error) {
+      console.log(error);
+    },
+    variables: {
+      postId,
+    },
+  });
+  return (
+    <>
+      <Button
+        as="div"
+        color="red"
+        floated="right"
+        onClick={() => setConfirmOpen(true)}
+      >
+        <Icon name="trash" style={{ margin: 0 }} />
+      </Button>
+      <Confirm
+        open={confirmOpen}
+        onCancel={() => setConfirmOpen(false)}
+        onConfirm={deletePost}
+      />
+    </>
+  );
+}
+
+const DELETE_POST_MUTATION = gql`
+  mutation deletePost($postId: ID!) {
+    deletePost(postId: $postId)
+  }
+`;
+export default DeleteButton;
--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 import { AuthContext } from '../context/auth';
 import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
 
 function PostCard({
   post: { body, createdAt, id, username, likeCount, commentCount, likes },
@@ -35,16 +36,7 @@ function PostCard({
             {commentCount}
           </Label>
         </Button>
-        {user && user.username === username && (
-          <Button
-            as="div"
-            color="red"
-            floated="right"
-            onClick={() => console.log('Delete post')}
-          >
-            <Icon name="trash" style={{ margin: 0 }} />
-          </Button>
-        )}
+        {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
   );
